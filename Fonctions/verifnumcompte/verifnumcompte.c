df8cdbfc1b487d19493805269455af38f43de8f5
GiuntaLucas <lucas-giunta@outlook.com>
2014-11-26 17:41:23 +0100

    verifnumcompte + fgetsperso

--- Fonctions/verifnumcompte/verifnumcompte.c
+++ Fonctions/verifnumcompte/verifnumcompte.c
@@ -1,104 +1,67 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int verifnumcompte (char* chaine);                         
-int verifnumcompte (char* chaine)
+int fgetsPerso(char *destination,int size);    
+int verifnumcompte (char* chaine);             
+int fgetsPerso(char *destination,int size)      /*Fonction qui fai en sorte que si on depasse le nombre de caractere de la saisie, ca nous mes une erreur*/
+{
+  int nombreCaracteres = 0;
+  int i;
+  destination[size-1]='\0';
+  char lettre;
+  do
+  {
+    lettre = getchar();
+    if(lettre!='\n')
+    {
+      destination[nombreCaracteres] = lettre;
+      nombreCaracteres++;
+    }
+    if(nombreCaracteres>size-1)
+    {
+      return 0;
+    }  
+  }
+  while(lettre != '\n');
+  return 1;
+}                         
+int verifnumcompte (char* chaine)                 /*Fonction de vérification de numéro de compte*/
 
 {
-  if(chaine[0]!='B')
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                 
-  }
-  if(chaine[1]!='E')
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[2]>57||chaine[2]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[3]>57||chaine[3]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[4]!=' ')
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[5]>57||chaine[5]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[6]>57||chaine[6]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[7]>57||chaine[7]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[8]>57||chaine[8]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[9]!=' ')
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[10]>57||chaine[5]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[11]>57||chaine[11]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[12]>57||chaine[12]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[13]>57||chaine[13]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[14]!=' ')
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[15]>57||chaine[15]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[16]>57||chaine[16]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[17]>57||chaine[17]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  if(chaine[18]>57||chaine[18]<48)
-  {
-   printf("Ce num%cro de compte est incorrect\n",130);
-   return 0;                  
-  }
-  return 1;
+    int i;
+    for(i=0;i<20;i++)
+    {
+                     if((chaine[0]!='B'||chaine[1]!='E'))
+                     {
+                     printf("Ce num%cro de compte est incorrect\n",130);
+                     return 0;                                 
+                     }
+                     if ((i==2||i==3||i==5||i==6||i==7||i==8||i==10||i==11||i==12||i==13||i==15||i==16||i==17||i==18)&&(chaine[i]<48||chaine[i]>57))
+                     {
+                     printf("Ce num%cro de compte est incorrect\n",130);
+                     return 0;                  
+            
+                     }
+                     if((i==4||i==9||i==14)&&(chaine[i]!=' '))
+                     {
+                     printf("Ce num%cro de compte est incorrect\n",130);
+                     return 0;                  
+                     }
+    return 1;
+    }
+}
+
+main()         /*Main pour vérifier que les fonctions fonctionnes*/
+{
+      char chaine[20] = "";
+      printf("Veuillez entrer votre num de compte : ");
+      if(fgetsPerso(chaine,sizeof(chaine))==1)            /*Pour faire appel a la fonction de verif de chaine, il faudra introduire le if a chaque foi*/
+      {
+        verifnumcompte(chaine);                            /*Ne pas oublier de changer le NOM de la fonction et le nom du tableau de verification a chaque foi (nom, prenom, adresse, etc etc*/
+      }                          
+      else
+      {
+        printf("erreur\n");
+      }                   
+      system("pause");
 }
