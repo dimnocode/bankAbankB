4201c4c86be487ba801be0b0c9ec52cb1c192c51
dimnocode <dimitri.krasucki@gmail.com>
2014-12-03 22:26:17 +0100

    Ajout fct dans projet complet

--- Fonctions/structclientAcopy/structclientAcopy.c
+++ Fonctions/structclientAcopy/structclientAcopy.c
@@ -9,16 +9,37 @@
 
 struct clientA 
 {
-int numero ;
-char nom [31];
-char prenom [31];
-char datenaiss [11] ;
-char num_compte [20] ;
-char num_reg_nat [14] ;
-};
+int numero;
+char nom[31];
+char prenom[31];
+char datenaiss[11] ;
+char num_compte[20];
+char num_reg_nat[14];
+}cA;
 
 
-void stringcopy() {  //Fonction de Murphy
+int strlength(char s[]){
+	
+	int i=0;
+	
+	while(s[i] != '\0') {
+		++i;
+	}
+	
+	return i;
+}
+	
+void stringcopy(char* source, char* destination){
+	
+	int i;
+	
+	for(i=0; i <= strlength(source); ++i){
+		
+		destination[i] = source[i];
+				
+	}
+	
+	
 }
 
 
@@ -38,5 +59,28 @@ void structclientAcopy(struct clientA source, struct clientA *destination){
 
 void main(){
 	
+	//struct clientA cA;
+	struct clientA cbis;
+	char test[10];
+	
+
+	
+	cA.numero = 123;
+	gets(cA.nom);
+	gets(cA.prenom);
+	gets(cA.datenaiss);
+	gets(cA.num_compte);
+	gets(cA.num_reg_nat);
+
+	
+	
+	structclientAcopy(cA, &cbis);
+	
+	printf("\n%s",cbis.nom);
+	printf("\n%s",cbis.prenom);
+	printf("\n%s",cbis.datenaiss);
+	printf("\n%s",cbis.num_compte);
+	printf("\n%s",cbis.num_reg_nat);
+
 
 }
